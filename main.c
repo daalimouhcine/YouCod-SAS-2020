a0da97205daa21256a3c674e6b4d2375c014ef53
Mouhcine Daali <the.daali.mouhcine@gmail.com>
2021-11-17 16:58:02 +0100

    tour II

--- main.c
+++ main.c
@@ -59,7 +59,7 @@ int main()
     printf("\n*********** Le Premier tour est commmencer ***********\n");
 
 
-    int isEqual = 1;
+    int isEqualI = 1;
     int votes[numPres];
 
     do{
@@ -84,15 +84,13 @@ int main()
 
 
 
-         int test;
          for(int i = 1 ; i < numPres ; i++){
-            test = votes[i];
-            if(test != votes[i-1]){
-                isEqual = 0;
+            if(votes[i] != votes[i-1]){
+                isEqualI = 0;
             }
         }
 
-        if(isEqual == 1 ){
+        if(isEqualI == 1 ){
             system("cls");
             printf("\n       ****** les presidents ******");
             for(int i = 0; i < numPres; i++){
@@ -106,33 +104,126 @@ int main()
         }
 
 
-    } while (isEqual == 1);
+    } while (isEqualI == 1);
 
 
 
-
-
-    system("cls");
-
-    printf("\n*********** les presidents qui en celectionner pour ld deuxeme toure ***********\n");
-
-    int numOfExites = 0;
+    int presExit[numPres];
 
     for(int i = 0 ; i < numPres ; i++){
-        if((votes[i]*100)/numElect <= 15){
-            votes[i]
+        if((votes[i]*100)/numElect >= 15){
+           presExit[i] = 1;
+
+        } else {
+            presExit[i] = 0;
         }
     }
 
+
+
+
+
+    // deuxieme tour:
+
+    system("cls");
+    printf("\n*********** les presidents qui en celectionner pour ld deuxieme toure ***********\n\n");
+    int n = 0;
+    int indexPresSecondTour[n];
     for(int i = 0 ; i < numPres ; i++){
-        printf("\nle president numero %d il va %d des votes.\n", presidents[i], votes[i]);
+
+        if(presExit[i] == 1){
+            n++;
+            printf("Le president numero %d est : %s.\n", n, presidents[i]);
+            indexPresSecondTour[n];
+
+        }
+    }
+    printf("\n*********** Le deuxieme tour est commmencer ***********\n");
+
+
+
+    char presidentsII[n][20];
+    int r = 0;
+    for(int i = 0 ; i < numPres ; i++){
+        if(presExit[i] == 1){
+           strcpy(presidentsII[r], presidents[i]);
+            r++;
+        }
+
     }
 
-    printf("\n*********** Le Premier tour est commmencer ***********\n");
+
+    int votesII[n];
+    int isEqualII = 1;
+    do{
+
+
+        for(int i = 0 ; i < n ; i++){
+            votesII[i] = 0;
+        }
 
 
 
-    // deuxeme tour:
+        for(int i = 0 ; i < numElect ; i++){
+            int v = 0 ;
+            printf("\nl'electeur N%d, qu'il est le president que tu veux le vote : ", i+1);
+            scanf("%d", &v);
+            while(v < 0 || v > n) {
+                printf("\nle numero de president que tu entrer est pas corect. \n\t\tessayer autre fois : ", i+1);
+                scanf("%d", &v);
+            }
+            votesII[v-1]++;
+        }
+
+
+
+        for(int i = 1 ; i < n ; i++){
+            if(votesII[i] != votesII[i-1]){
+                isEqualII = 0;
+            }
+        }
+
+        if(isEqualII == 1 ){
+            system("cls");
+            printf("\n       ****** les presidents ******");
+            for(int i = 0; i < n; i++){
+                printf("\nLe president numero %d est : %s .", i+1, presidentsII[i]);
+
+            }
+            printf("\n           ****** ****** ******");
+            printf("\n ***** tout les presidents est le meme vote, en a commencer auter fois. ***** \n");
+
+        } else {
+            break;
+        }
+
+
+    } while(isEqualII = 1);
+
+
+
+    int presExitII[n];
+    int min, posMin;
+    for(int i = 0 ; i < n ; i++){
+        min = votesII[i];
+        if(min < votes[i]){
+            min = votes[i];
+            posMin = i;
+        }
+    }
+    int numMin = 0;
+    for(int i = 0; i < n ; i++){
+
+    }
+
+
+
+
+
+    // troixieme tour:
+
+
+
 
     return 0;
 }
